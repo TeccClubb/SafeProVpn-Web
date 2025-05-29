04df1ef39258d462b4fdc2efe4768ad8a2382323
altafkhankpk <145385734+altafkhankpk@users.noreply.github.com>
2025-05-29 18:42:43 +0500

    designed feature page part one

---
+++ src/app/feature/page.tsx
@@ -0,0 +1,13 @@
+
+import UltimatePrivacy from '@/components/feature/ultimate';
+import React from 'react'
+
+const Feature = () => {
+  return (
+    <div>
+      <UltimatePrivacy></UltimatePrivacy>
+    </div>
+  )
+}
+
+export default Feature;
---
+++ src/components/feature/ultimate.tsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import Section from '../sections/Section'
+import Button from '../Button/button'
+
+const UltimatePrivacy = () => {
+  return (
+    <Section heading={<>Advanced Features for <br></br><span className='text-primary'>Ultimate Privacy</span></>}
+    description="SafePro VPN provides top-tier security features to keep your online activities private and your data protected, no matter where you are.">
+    <div className='flex justify-center'>
+     <Button>Get Free Trial</Button>
+     <Button className='ml-4' variant='outline'>Learn More</Button>
+    </div>
+    </Section>
+  )
+}
+
+export default UltimatePrivacy
