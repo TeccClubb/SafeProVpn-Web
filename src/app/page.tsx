f2f14afc9d12f041cedb70d91976c9785feb6c87
M SAQIB ALI <MSaqibAliISF@gmail.com>
2025-06-25 16:06:46 +0500

    feat: Add UsersRemarks and Pricing sections to HomePage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
 "use client";
 
-import HomePage from "@/components/Home/home";
- 
+import React, { FC } from "react";
+import VPNPromo from "@/components/SafeProVpn-home/securePrivacy";
+import WhyChooseSection from "@/components/SafeProVpn-home/WhyChooseSection";
+import SecureEveryClickSection from "@/components/SafeProVpn-home/secure-every-click";
+import SafeProWork from "@/components/SafeProVpn-home/safePro-work";
+import AvailableDevices from "@/components/SafeProVpn-home/available-devices";
+import UsersRemarksSection from "@/components/SafeProVpn-home/UsersRemarksSection";
+import PricingSection from "@/components/SafeProVpn-home/PricingSection";
+import FrequentlyQuestion from "@/components/SafeProVpn-home/frequently-question";
 
-export default function Home() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
-}
+const HomePage: FC = () => (
+  <>
+    <VPNPromo />
+    <WhyChooseSection />
+    <SecureEveryClickSection />
+    <SafeProWork />
+    <AvailableDevices />
+    <UsersRemarksSection />
+    <PricingSection />
+    <FrequentlyQuestion />
+  </>
+);
+
+export default HomePage;
