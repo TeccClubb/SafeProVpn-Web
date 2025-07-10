af0cf516599ef016ca494a889042b01721abf77c
M Saqib Ali <MSaqibAliISF@gmail.com>
2025-07-10 07:24:10 +0500

    feat: Replace axios with fetch for IP address retrieval in utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,19 +1,17 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { osName, browserName } from "react-device-detect";
-import axios from "axios";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 export const getIPAddress = async () => {
-  const res = await axios
-    .get<{ ip: string }>("https://api.ipify.org?format=json", {
-      headers: { Accept: "application/json" },
-    })
-    .then((res) => res.data);
-  return res.ip;
+  const res = await fetch("https://api.ipify.org?format=json", {
+    headers: { Accept: "application/json" },
+  });
+  const data = await res.json();
+  return await data.ip;
 };
 
 export const deviceInfo = {
